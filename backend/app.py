8a36e155c936dbb92b98b795f58055cc485fde2e
SuruiLiu <742901100@qq.com>
2025-08-22 18:09:33 +1000

    feat: update docker and page

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from config.config import Config
 from controllers.home_controller import home_bp
+from controllers.pdf_extraction_controller import pdf_extraction_bp
 
 def create_app():
     """Create Flask application"""
@@ -13,6 +14,7 @@ def create_app():
     
     # Register blueprints
     app.register_blueprint(home_bp)
+    app.register_blueprint(pdf_extraction_bp)
     
     return app
 
